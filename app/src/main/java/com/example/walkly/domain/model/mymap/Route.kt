fc684955f30cf93b00962400ff6d5e46f8002da1
ritsuki990907 <ritsuki990907@gmail.com>
2021-07-08 22:30:13 +0900

    HTTPリクエストの同期処理 #45 #33

--- app/src/main/java/com/example/walkly/domain/model/mymap/Route.kt
+++ app/src/main/java/com/example/walkly/domain/model/mymap/Route.kt
@@ -10,6 +10,8 @@ import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.maps.android.PolyUtil
 import org.json.JSONObject
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
 
 class Route(private val mMap: GoogleMap) {
 
@@ -20,46 +22,50 @@ class Route(private val mMap: GoogleMap) {
      * @param place 中間地点,終点
      * @throws Exception APIキーが間違っているなどのエラー
      */
-    fun drawRoute(origin: LatLng, place: MutableList<LatLng>) {
+    suspend fun drawRoute(origin: LatLng, place: MutableList<LatLng>): Boolean {
 
         // TODO: 正式リリース時に消す
-        if (false) {
+        return suspendCoroutine { continuation ->
+            if (false) {
+                val path: MutableList<List<LatLng>> = ArrayList()
+                val urlDirections = createURLDirections(origin, place)
 
-            val path: MutableList<List<LatLng>> = ArrayList()
-            val urlDirections = createURLDirections(origin, place)
+                val listener = Response.Listener<String> { response ->
+                    val jsonResponse = JSONObject(response)
+                    try {
+                        val routes = jsonResponse.getJSONArray("routes")
+                        val legs = routes.getJSONObject(0).getJSONArray("legs")
 
-            val listener = Response.Listener<String> { response ->
-                val jsonResponse = JSONObject(response)
-                try {
-                    val routes = jsonResponse.getJSONArray("routes")
-                    val legs = routes.getJSONObject(0).getJSONArray("legs")
+                        for (j in 0 until legs.length()) {
 
-                    for (j in 0 until legs.length()) {
+                            val steps = legs.getJSONObject(j).getJSONArray("steps")
+                            for (i in 0 until steps.length()) {
+                                val points =
+                                    steps.getJSONObject(i).getJSONObject("polyline")
+                                        .getString("points")
+                                path.add(PolyUtil.decode(points))
+                            }
+                            for (i in 0 until path.size) {
+                                mMap.addPolyline(
+                                    PolylineOptions().addAll(path[i]).color(Color.BLUE)
+                                )
+                            }
 
-                        val steps = legs.getJSONObject(j).getJSONArray("steps")
-                        for (i in 0 until steps.length()) {
-                            val points =
-                                steps.getJSONObject(i).getJSONObject("polyline")
-                                    .getString("points")
-                            path.add(PolyUtil.decode(points))
                         }
-                        for (i in 0 until path.size) {
-                            mMap.addPolyline(
-                                PolylineOptions().addAll(path[i]).color(Color.BLUE)
-                            )
-                        }
 
+                    } catch (e: Exception) {
+                        throw Exception(jsonResponse.getString("error_message"))
+                    }  finally {
+                        continuation.resume(true)
                     }
-
-                } catch (e: Exception) {
-                    throw Exception(jsonResponse.getString("error_message"))
                 }
+
+                val errorListener = Response.ErrorListener {
+                    continuation.resume(false)
+                }
+                HTTPRequest().getRequest(urlDirections, listener, errorListener)
             }
-
-            val errorListener = Response.ErrorListener {  }
-            HTTPRequest().getRequest(urlDirections, listener, errorListener)
         }
-
     }
 
     /**
